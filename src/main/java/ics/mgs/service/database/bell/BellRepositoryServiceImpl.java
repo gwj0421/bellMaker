fc00c016bea6132c395f73ad244c7c72c6bf7825
gunwoong <gwj0421@gmail.com>
2023-12-18 16:52:24 +0900

    feat : 에러 처리 추가
    없는 유저를 조회할 경우 에러 처리

--- src/main/java/ics/mgs/service/database/bell/BellRepositoryServiceImpl.java
+++ src/main/java/ics/mgs/service/database/bell/BellRepositoryServiceImpl.java
@@ -3,11 +3,13 @@ package ics.mgs.service.database.bell;
 import ics.mgs.dao.Bell;
 import ics.mgs.dao.SiteUser;
 import ics.mgs.dto.FileResponse;
+import ics.mgs.error.UserNotFound;
 import ics.mgs.repository.BellRepository;
 import ics.mgs.repository.SiteUserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
@@ -24,22 +26,24 @@ public class BellRepositoryServiceImpl implements BellRepositoryService {
     }
 
     @Override
+    @Transactional
     public SiteUser saveBellToUser(String userId, FileResponse response) {
         Optional<SiteUser> user = userRepository.findSiteUserByUserId(userId);
         if (user.isEmpty()) {
             log.info("gwj : cannot find user in user repository");
-            return SiteUser.builder().build();
+            throw new UserNotFound();
         }
 
         Set<Bell> bells = new LinkedHashSet<>();
         for (int i = 0; i < response.getContent().size(); i++) {
             List<String> pathAndUri = response.getContent().get(i);
-            Bell bell = Bell.builder().fileName(pathAndUri.get(0)).user(user.get())
+            Bell bell = Bell.builder().fileName(pathAndUri.get(0))
                     .url(pathAndUri.get(1))
                     .build();
+            user.get().addBell(bell);
             bells.add(bell);
         }
         bellRepository.saveAll(bells);
-        return userRepository.save(user.get());
+        return user.get();
     }
 }
