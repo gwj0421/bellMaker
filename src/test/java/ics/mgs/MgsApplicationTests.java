17d58302d7618fab68881e19d5589ae97d7c6f3a
gunwoong <gwj0421@gmail.com>
2023-12-18 16:56:42 +0900

    test : 기능과 상관없는 코드 삭제

--- src/test/java/ics/mgs/MgsApplicationTests.java
+++ src/test/java/ics/mgs/MgsApplicationTests.java
@@ -3,22 +3,10 @@ package ics.mgs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootTest
 class MgsApplicationTests {
-
     @Test
     void contextLoads() {
-        List<String> test = new ArrayList<>();
-        test.add("a");
-        test.add("b");
-        test.add(null);
-        test.add("c");
-        test.add("d");
-
-        System.out.println(String.join("|", test));
     }
 
 }
