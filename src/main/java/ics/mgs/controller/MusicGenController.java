7cb6aa0079968e14e23486d13ec9d3bce5524054
gunwoong <gwj0421@gmail.com>
2023-12-18 16:58:41 +0900

    feat : 유저에게 Bell 저장 및 Bell 제공

--- src/main/java/ics/mgs/controller/MusicGenController.java
+++ src/main/java/ics/mgs/controller/MusicGenController.java
@@ -2,11 +2,14 @@ package ics.mgs.controller;
 
 import ics.mgs.dto.FileResponse;
 import ics.mgs.dto.InputForm;
-import ics.mgs.service.musicgen_service.MusicGenServiceImpl;
+import ics.mgs.service.database.bell.BellRepositoryService;
+import ics.mgs.service.database.site_user.SiteUserRepositoryService;
+import ics.mgs.service.musicgen_service.MusicGenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +18,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @RequiredArgsConstructor
 public class MusicGenController {
-    private final MusicGenServiceImpl musicGenService;
+    private final MusicGenService musicGenService;
+    private final SiteUserRepositoryService userRepositoryService;
+    private final BellRepositoryService bellRepositoryService;
 
     @PostMapping("/sendInfoToModel/{userId}")
     public String sendToModel(@ModelAttribute InputForm inputForm, @PathVariable String userId, Model model) {
         FileResponse response = musicGenService.sendClueToModelServer(userId, inputForm);
+
+        bellRepositoryService.saveBellToUser(userId, response);
+
         if (response.getStatus().equals("OK")) {
             model.addAttribute("response", response);
         } else {
             model.addAttribute("response", FileResponse.makeEmptyResponse());
         }
-        return "audio_form";
+        return "redirect:/showMusic/" + userId;
+    }
+
+    @GetMapping("/showMusic/{userId}")
+    public String showMusic(@PathVariable String userId, Model model) {
+        model.addAttribute("userContent", userRepositoryService.getSiteUserByUserId(userId).getBells());
+
+        return "user_audio_form";
     }
 }
