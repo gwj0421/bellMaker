a5c60aaacec05b3336c6fbf1dcdfee586194127d
gunwoong <gwj0421@gmail.com>
2023-12-19 14:29:42 +0900

    feat : Bell이 만료 처리 기능 서비스 코드 확대

--- src/main/java/ics/mgs/service/database/bell/BellRepositoryService.java
+++ src/main/java/ics/mgs/service/database/bell/BellRepositoryService.java
@@ -1,8 +1,17 @@
 package ics.mgs.service.database.bell;
 
+import ics.mgs.dao.Bell;
 import ics.mgs.dao.SiteUser;
 import ics.mgs.dto.FileResponse;
 
+import java.util.List;
+
 public interface BellRepositoryService {
     public SiteUser saveBellToUser(String userId, FileResponse response);
+
+    public List<Bell> findBellsWithFetchJoin();
+
+    public void saveBell(Bell bell);
+
+    public void deleteBellByExpired();
 }
